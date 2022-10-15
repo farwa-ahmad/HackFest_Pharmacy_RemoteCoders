b030b5296bcc22d05f81803f43499dc813682ea0
Mehuol Dhanji <mehuol.dhanji@simpaisa.com>
2022-10-15 22:47:38 +0500

    menu search btn added

--- includes/main.php
+++ includes/main.php
@@ -10,13 +10,11 @@
         <div class="currency">
           <a class="currency__change" href="customer/my_account.php?my_orders">
           <?php
-          if(!isset($_SESSION['customer_email'])){
-          echo "Welcome :Guest"; 
-          }
-          else
-          { 
-              echo "Welcome : " . $_SESSION['customer_email'] . "";
-            }
+if (!isset($_SESSION['customer_email'])) {
+    echo "Welcome :Guest";
+} else {
+    echo "Welcome : " . $_SESSION['customer_email'] . "";
+}
 ?>
           </a>
         </div>
@@ -24,40 +22,36 @@
         <div class="basket">
           <a href="cart.php" class="btn btn--basket">
             <i class="icon-basket"></i>
-            <?php items(); ?> items
+            <?php items();?> items
           </a>
         </div>
-        
-        
+
+
         <ul class="login">
 
 <li class="login__item">
 <?php
-if(!isset($_SESSION['customer_email'])){
-  echo '<a href="customer_register.php" class="login__link">Register</a>';
-} 
-  else
-  { 
-      echo '<a href="customer/my_account.php?my_orders" class="login__link">My Account</a>';
-  }   
-?>  
+if (!isset($_SESSION['customer_email'])) {
+    echo '<a href="customer_register.php" class="login__link">Register</a>';
+} else {
+    echo '<a href="customer/my_account.php?my_orders" class="login__link">My Account</a>';
+}
+?>
 </li>
 
 
 <li class="login__item">
 <?php
-if(!isset($_SESSION['customer_email'])){
-  echo '<a href="checkout.php" class="login__link">Sign In</a>';
-} 
-  else
-  { 
-      echo '<a href="./logout.php" class="login__link">Logout</a>';
-  }   
-?>  
-  
+if (!isset($_SESSION['customer_email'])) {
+    echo '<a href="checkout.php" class="login__link">Sign In</a>';
+} else {
+    echo '<a href="./logout.php" class="login__link">Logout</a>';
+}
+?>
+
 </li>
 </ul>
-      
+
       </div>
     </div>
     <!-- bottomline -->
@@ -75,7 +69,7 @@ if(!isset($_SESSION['customer_email'])){
             <li class="categories__item">
               <a class="categories__link" href="about.php">
                 About
-               
+
               </a>
             </li>
 
@@ -121,18 +115,26 @@ if(!isset($_SESSION['customer_email'])){
                       <li class="dropdown__item">
                         <a href="#" class="dropdown__link">Change Password</a>
                       </li>
-                      <!-- <li class="dropdown__item">
-                        <a href="#" class="dropdown__link">Delete Account</a>
-                      </li> -->
+                      
                     </ul>
                   </div>
                 </div>
-             
+
 
               </div>
 
             </li>
 
+<li class="categories__item">
+                        <div class="wrap">
+                        <div class="search">
+                              <input type="text" class="searchTerm" placeholder="Search">
+                              <button type="submit" class="searchButton">
+                                <i class="fa fa-search"></i>
+                            </button>
+                          </div>
+                        </div>
+                      </li>
           </ul>
         </nav>
       </div>
