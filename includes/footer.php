11757bdc232379d82b247867799b662472d671fb
Sania-Iftikhar <saniaiftikhar27@gmail.com>
2022-10-15 10:11:55 -0700

    Footer Updated

--- includes/footer.php
+++ includes/footer.php
@@ -6,11 +6,11 @@
 			<div class="footer-nav__col footer-nav__col--info">
 				<div class="footer-nav__heading">Information</div>
 				<ul class="footer-nav__list">
+					<!-- <li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Order and Delivery Support</a>
+					</li> -->
 					<li class="footer-nav__item">
-						<a href="#" class="footer-nav__link">The brand</a>
-					</li>
-					<li class="footer-nav__item">
-						<a href="#" class="footer-nav__link">Local stores</a>
+						<a href="#" class="footer-nav__link">Technical Support</a>
 					</li>
 					<li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">Customer service</a>
@@ -25,20 +25,20 @@
 			</div>
 
 			<div class="footer-nav__col footer-nav__col--whybuy">
-				<div class="footer-nav__heading">Why buy from us</div>
+				<div class="footer-nav__heading">WHY US</div>
 				<ul class="footer-nav__list">
-					<li class="footer-nav__item">
+					<!-- <li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">Shipping &amp; returns</a>
-					</li>
+					</li> -->
 					<li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">Secure shipping</a>
 					</li>
 					<li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">Testimonials</a>
 					</li>
-					<li class="footer-nav__item">
+					<!-- <li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">Award winning</a>
-					</li>
+					</li> -->
 					<li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">Ethical trading</a>
 					</li>
@@ -48,18 +48,18 @@
 			<div class="footer-nav__col footer-nav__col--account">
 				<div class="footer-nav__heading">Your account</div>
 				<ul class="footer-nav__list">
-					<li class="footer-nav__item">
+					<!-- <li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">Sign in</a>
 					</li>
 					<li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">Register</a>
-					</li>
+					</li> -->
 					<li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">View cart</a>
 					</li>
-					<li class="footer-nav__item">
+					<!-- <li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">View your lookbook</a>
-					</li>
+					</li> -->
 					<li class="footer-nav__item">
 						<a href="#" class="footer-nav__link">Track an order</a>
 					</li>
@@ -118,16 +118,16 @@
 		<div class="container clearfix">
 
 			<div class="copyright">
-				&copy; 2018 Avenue Fashion&trade;
+				&copy; 2022 HackFest&trade;
 			</div>
 
 			<div class="developer">
-				Dev by Yasser Dalouzi
+				Designed &amp; Developed by Remote Coders
 			</div>
 
-			<div class="designby">
-				Design by Yasser Dalouzi
-			</div>
+			<!-- <div class="designby">
+				Design by Remote Coders
+			</div> -->
 		</div>
 	</div>
 </footer>
